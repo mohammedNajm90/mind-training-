539e57240c5ed452b1a79d00a49bbe8e5fa98220
MoOsA5 <moosasumer01@gmail.com>
2025-07-28 20:04:22 +0300

    solve more tasks

---
+++ OOPExcercises/OOPExcercises/Shape.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPExcercises
+{
+    class Shape
+    {
+        public virtual double Area()
+        {
+            return 0;
+        }
+    }
+
+    class Circle : Shape
+    {
+        public double Radius { get; set; }
+
+        public Circle(double radius)
+        {
+            Radius = radius;
+        }
+
+        public override double Area()
+        {
+            return Math.PI * Radius * Radius;
+        }
+    }
+
+    class Square : Shape
+    {
+        public double Side { get; set; }
+
+        public Square(double side)
+        {
+            Side = side;
+        }
+
+        public override double Area()
+        {
+            return Side * Side;
+        }
+    }
+}

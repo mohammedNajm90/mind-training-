0151fd3a185d8a4971d6f351b4b40be2361dbe80
MoOsA5 <moosasumer01@gmail.com>
2025-08-23 18:27:12 +0300

    File system Exercise

---
+++ FileSystem/FileSystem/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+
+namespace FileSystem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String path1 = @"C:\Users\LENOVO\source\repos\FileSystem\FileSystem\Folder1\File1.txt";
+            String path2 = @"C:\Users\LENOVO\source\repos\FileSystem\FileSystem\Folder2\File2.txt";
+            String path3 = @"C:\Users\LENOVO\source\repos\FileSystem\FileSystem\Folder2\File3.txt";
+
+            String directoryPath1 = @"C:\Users\LENOVO\source\repos\FileSystem\FileSystem\Folder1\";
+
+            Solutions.WriteToFile("Hello File System",path1);
+            Solutions.ReadFromFile(path1);
+            Solutions.AppendAllText(path1, " New Line Added");
+            Solutions.ReadFromFile(path1);
+            Solutions.Copy(path1, path2);
+            Solutions.ReadFromFile(path2);
+            //Solutions.Move(path1, path3);
+            //Solutions.ReadFromFile(path3);
+            //Solutions.Delete(path1);
+            Solutions.GetFiles(directoryPath1);
+            Console.WriteLine();
+            Solutions.GetTxtFiles(directoryPath1);
+            Solutions.Counts(path1);
+        }
+    }
+}
---
+++ FileSystem/FileSystem/Solutions.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.IO;
+
+namespace FileSystem
+{
+    class Solutions
+    {
+        //1.Write a program to create a text file and write "Hello File System" into it.
+        public static void WriteToFile(String text, String fileName)
+        {
+            File.WriteAllText(fileName, text);
+        }
+
+        //2.Read the content of a text file and print it line by line. 
+        public static void ReadFromFile(String fileName)
+        {
+            string readText = File.ReadAllText(fileName);
+            Console.WriteLine(readText);
+        }
+
+        //3.Append "New Line Added" to an existing file without overwriting it.
+        public static void AppendAllText(string path, string contents)
+        {
+            File.AppendAllText(path, contents);
+        }
+
+        //4.Copy a file from one directory to another.
+        public static void Copy(string sourceFileName, string destFileName)
+        {
+            File.Copy(sourceFileName, destFileName,true);
+        }
+
+        //5.Move a file to a new folder and rename it
+        public static void Move(string sourceFileName, string destFileName)
+        {
+            File.Move(sourceFileName, destFileName);
+        }
+
+        //6.Delete a file safely (no exception if the file does not exist)
+        public static void Delete(string path)
+        {
+            File.Delete(path);
+        }
+
+        //7.List all files in a given directory and print their names.
+        public static void GetFiles(string directoryPath)
+        {
+            string[] files = Directory.GetFiles(directoryPath);
+            foreach (string fileName in files)
+                Console.WriteLine(fileName.Substring(fileName.LastIndexOf(@"\")+1));
+        }
+
+        //8.List all.txt files in a directory (and subdirectories).
+        public static void GetTxtFiles(string directoryPath)
+        {
+            string[] files = Directory.GetFiles(directoryPath,"*.txt");
+            foreach (string fileName in files)
+                Console.WriteLine(fileName.Substring(fileName.LastIndexOf(@"\") + 1));
+        }
+
+        //10.Write a program that counts how many words are in a file.
+        public static void Counts(String path)
+        {
+            string readText = File.ReadAllText(path);
+            String[] words;
+            words = readText.Split(' ');
+            int count = 0;
+            foreach (var word in words)
+            {
+                count++;
+            }
+            Console.WriteLine("Number of words: " + count);
+            
+        }
+
+    }
+}
